e8fbbdaca87d3e280c80938e0529c78c1c93ebb2
Denis Volkov <to.den.volkov@gmail.com>
2021-10-20 19:12:42 +0500

    Сделал форму на фронте для расчетов

--- src/routes/RouterV2.ts
+++ src/routes/RouterV2.ts
@@ -4,11 +4,11 @@ function calculateHandlerV2(params: any) {
     return new CalculateHandler().handle(params)
 }
 
-const RouterV1 = express.Router();
-RouterV1.get( "/calc", ( req, res ) => {
+const RouterV2 = express.Router();
+RouterV2.get( "/calc", ( req, res ) => {
     const result = calculateHandlerV2(req.query);
     // render the index template
     res.json({result});
 } );
 
-export = RouterV1;
+export = RouterV2;
